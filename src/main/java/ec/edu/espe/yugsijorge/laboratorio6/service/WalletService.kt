a6c8121ffb48dde6789f922dbce791232eef72ee
JLYugsi <jlyugsi@espe.edu.ec>
2026-07-17 16:45:49 -0500

    Commit inicial con proyecto base y workflows

---
+++ src/main/java/ec/edu/espe/yugsijorge/laboratorio6/service/WalletService.kt
@@ -0,0 +1,4 @@
+package ec.edu.espe.yugsijorge.laboratorio6.service
+
+class WalletService {
+}
---
+++ src/test/java/ec/edu/espe/yugsijorge/laboratorio6/service/WalletServiceTest.java
@@ -0,0 +1,4 @@
+package ec.edu.espe.yugsijorge.laboratorio6.service;
+
+public class WalletServiceTest {
+}
